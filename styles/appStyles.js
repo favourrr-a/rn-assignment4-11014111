bcf575eb57183ca85c908e2bcd9e788c0a072402
favour <fackonu@st.ug.edu.gh>
2024-06-14 18:59:44 +0000

    Chore: Add appStyles.js for styling App component
    I removed some default stuff from App.js too

---
+++ styles/appStyles.js
@@ -0,0 +1,11 @@
+// React Native components
+import { StyleSheet } from 'react-native';
+
+export const appStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
