088dfde914f395184149a4fb80ea90ec4556b30d
favour <fackonu@st.ug.edu.gh>
2024-06-18 23:29:20 +0000

    Add styles to the Home page

--- pages/Home.js
+++ pages/Home.js
@@ -3,13 +3,14 @@ import { View, Text } from 'react-native';
 
 // Styles
 import { appStyles } from '../styles/appStyles';
+import { homeStyles } from '../styles/homeStyles';
 
 export default function Home({route}) {
     const {name, email} = route.params;
     console.log(route);
     return (
         <View style = {appStyles.container}>
-        <View>
+        <View style = {homeStyles.homePageContainer}>
             <Text>Home</Text>
         </View>
         </View>
---
+++ styles/homeStyles.js
@@ -0,0 +1,11 @@
+// React Native Components
+import { StyleSheet } from 'react-native';
+
+export const homeStyles = StyleSheet.create({
+    homePageContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: '90%',
+        width: '90%',
+    },
+});
